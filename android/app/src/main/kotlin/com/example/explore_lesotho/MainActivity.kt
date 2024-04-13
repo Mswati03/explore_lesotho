68a81f4294fe07699b2eecaa0b8df731eca95e02
Mswati03 <mswatitshabalala03@gmail.com>
2024-04-13 07:11:19 -0700

    Code upload

---
+++ android/app/src/main/kotlin/com/example/explore_lesotho/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.explore_lesotho
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/homepage.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget
+{
+  const HomePage({
+    super.key
+    });
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold (
+                     appBar: AppBar(
+                      title: const Text("Explore Lesotho",),
+                      ),
+                    body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                const Spacer(),
+                Image.asset(
+                  'places/Sani+Pass.jpg',
+                ),
+                const SizedBox(height: 40),
+                const Text(
+                  "Explore your journey \nonly with us",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  "All your vacations destinations are here,\nenjoy your holiday",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 16,
+                  ),
+                ),
+                const Spacer(),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 30),
+                  child: SizedBox(
+                    width: double.maxFinite,
+                    child: ElevatedButton(
+                      onPressed: () {
+                      showDialog(
+                context: context,
+                builder: (ctx) => AlertDialog(
+                  title: const Text("Alert Dialog Box"),
+                  content: const Text("Loading Soon"),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(ctx).pop();
+                      },
+                      child: Container(
+                        color: Colors.green,
+                        padding: const EdgeInsets.all(14),
+                        child: const Text("okay"),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        elevation: 0,
+                        shape: const StadiumBorder(),
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 15,
+                          horizontal: 8.0,
+                        ),
+                      ),
+                      child: const Text("Get Started"),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+
+}
