e238445943c6ad0fe8afa3e59597542f9b1e1d24
Mswati03 <mswatitshabalala03@gmail.com>
2024-04-16 19:44:20 -0700

    homepage updates

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:stroke_text/stroke_text.dart';
 
 class HomePage extends StatelessWidget
 {
@@ -10,36 +15,100 @@ class HomePage extends StatelessWidget
   Widget build(BuildContext context) {
     return Scaffold (
                      appBar: AppBar(
-                      title: const Text("Explore Lesotho",),
+                      title: const Center(
+                      child :Text("Explore Lesotho",),
+
                       ),
-                    body: SafeArea(
-        child: Center(
+                       backgroundColor: Colors.transparent,
+                    ),
+                    body: Stack(
+        children: [
+          Container(
+height: double.infinity,
+                    width: double.infinity,
+                    decoration : BoxDecoration(
+                    image: DecorationImage(
+                        image : AssetImage(
+              'places/Sani+Pass.jpg',),
+              fit: BoxFit.cover
+          ),
+          ),
+            child: new BackdropFilter(
+              filter: new ImageFilter.blur(sigmaX: 2.0, sigmaY: 2.0),
+              child: new Container(
+                decoration: new BoxDecoration(color: Colors.white.withOpacity(0.0)),
+              ),
+            ),
+          ),
+
+          Center(
           child: Padding(
-            padding: const EdgeInsets.all(16),
+            padding: const EdgeInsets.all(1),
             child: Column(
+
               children: [
                 const Spacer(),
-                Image.asset(
-                  'places/Sani+Pass.jpg',
+                Container(
+
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(2000000000000.0),
+                    child :Image(
+                      image :AssetImage(
+                          'logo/explore_logo.png',),
+                        filterQuality: FilterQuality.none,
+                        fit: BoxFit.none,
+
+                    ),
+
+                  ),
                 ),
-                const SizedBox(height: 40),
+
                 const Text(
-                  "Explore your journey \nonly with us",
+                  "Explore Lesotho \nonly with us",
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 26,
-                    fontWeight: FontWeight.bold,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.white70,
+                   fontFamily: 'Inter-black',
                   ),
+                ),
+                Stack(
+                  children: <Widget>[
+                    // Stroked text as border.
+                    Text(
+                      'Greetings, planet!',
+                      style: TextStyle(
+                        fontSize: 30,
+                        foreground: Paint()
+                          ..style = PaintingStyle.stroke
+                          ..strokeWidth = 6
+                          ..color = Colors.black,
+                      ),
+                    ),
+                    // Solid text as fill.
+                    Text(
+                      'Greetings, planet!',
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
                 ),
                 const SizedBox(height: 20),
-                const Text(
-                  "All your vacations destinations are here,\nenjoy your holiday",
+                 Text(
+                  "All your vacations destinations are here,\nenjoy your",
                   textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.black54,
+                  style: GoogleFonts.inter(
+                   textStyle: TextStyle(
+                    color: Colors.white,
                     fontSize: 16,
+
+                  ),
                   ),
                 ),
+
                 const Spacer(),
                 Padding(
                   padding: const EdgeInsets.only(bottom: 30),
@@ -83,6 +152,7 @@ class HomePage extends StatelessWidget
             ),
           ),
         ),
+    ],
       ),
     );
   }
