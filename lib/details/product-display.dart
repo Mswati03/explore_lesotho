d01a0dd975a23da90400ea2ae9ea14dbc598816a
Mswati03 <mswatitshabalala03@gmail.com>
2024-05-27 19:28:06 -0700

    select-dates

--- lib/details/product-display.dart
+++ lib/details/product-display.dart
@@ -1,10 +1,115 @@
+import 'package:calendar_date_picker2/calendar_date_picker2.dart';
 import 'package:flutter/material.dart';
 
-class SantoriniIslandPage extends StatelessWidget {
+class SantoriniIslandPage extends StatefulWidget {
+
+  @override
+  State<SantoriniIslandPage> createState() => _SantoriniIslandPageState();
+}
+
+class _SantoriniIslandPageState extends State<SantoriniIslandPage> {
+   DateTime selectedDate = DateTime.now();
+   bool dateSelected = false;
+   //bool dateSelected = false;
+  String dateResponse = '';
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime(2015, 8),
+        lastDate: DateTime(2101));
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
+  }
+
+  List<DateTime?> _rangeDatePickerWithActionButtonsWithValue = [
+    DateTime.now(),
+    DateTime.now().add(const Duration(days: 5)),
+  ];
+   List<DateTime?> _dialogCalendarPickerValue = [
+    DateTime(2021, 8, 10),
+    DateTime(2021, 8, 13),
+  ];
+
+  
+
   @override
   Widget build(BuildContext context) {
-    var firstDate;
-    var lastDate;
+
+    String _getValueText(
+    CalendarDatePicker2Type datePickerType,
+    List<DateTime?> values,
+  ) {
+    values =
+        values.map((e) => e != null ? DateUtils.dateOnly(e) : null).toList();
+    var valueText = (values.isNotEmpty ? values[0] : null)
+        .toString()
+        .replaceAll('00:00:00.000', '');
+
+    if (datePickerType == CalendarDatePicker2Type.multi) {
+      valueText = values.isNotEmpty
+          ? values
+              .map((v) => v.toString().replaceAll('00:00:00.000', ''))
+              .join(', ')
+          : 'null';
+    } else if (datePickerType == CalendarDatePicker2Type.range) {
+      if (values.isNotEmpty) {
+        final startDate = values[0].toString().replaceAll('00:00:00.000', '');
+        final endDate = values.length > 1
+            ? values[1].toString().replaceAll('00:00:00.000', '')
+            : 'null';
+        valueText = '$startDate to $endDate';
+      } else {
+        return 'null';
+      }
+    }
+
+    return valueText;
+  }
+
+
+    Widget _buildCalendarWithActionButtons() {
+    final config = CalendarDatePicker2WithActionButtonsConfig(
+      calendarType: CalendarDatePicker2Type.range,
+      disableModePicker: true,
+    );
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const SizedBox(height: 10),
+        const Text('Date Picker With Action Buttons'),
+        CalendarDatePicker2WithActionButtons(
+          config: config,
+          value: _rangeDatePickerWithActionButtonsWithValue,
+          onValueChanged: (dates) => setState(
+              () => _rangeDatePickerWithActionButtonsWithValue = dates),
+        ),
+        const SizedBox(height: 10),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text('Selection(s):  '),
+            const SizedBox(width: 10),
+            Flexible(
+              child: Text(
+                _getValueText(
+                  config.calendarType,
+                  _rangeDatePickerWithActionButtonsWithValue,
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 25),
+      ],
+    );
+  }
+    
+   
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
@@ -55,7 +160,158 @@ class SantoriniIslandPage extends StatelessWidget {
                         fontWeight: FontWeight.w600,
                       ),
                     ),
-                    DatePickerDialog(firstDate: firstDate, lastDate: lastDate),
+                    Center(
+            child: ElevatedButton(
+            onPressed: () async {
+              const dayTextStyle =
+        TextStyle(color: Colors.black, fontWeight: FontWeight.w700);
+    final weekendTextStyle =
+        TextStyle(color: Colors.grey[500], fontWeight: FontWeight.w600);
+    final anniversaryTextStyle = TextStyle(
+      color: Colors.red[400],
+      fontWeight: FontWeight.w700,
+      decoration: TextDecoration.underline,
+    );
+              final config = CalendarDatePicker2WithActionButtonsConfig(
+      calendarViewScrollPhysics: const NeverScrollableScrollPhysics(),
+      dayTextStyle: dayTextStyle,
+      calendarType: CalendarDatePicker2Type.range,
+      selectedDayHighlightColor: Colors.purple[800],
+      closeDialogOnCancelTapped: true,
+      firstDayOfWeek: 1,
+      weekdayLabelTextStyle: const TextStyle(
+        color: Colors.black87,
+        fontWeight: FontWeight.bold,
+      ),
+      controlsTextStyle: const TextStyle(
+        color: Colors.black,
+        fontSize: 15,
+        fontWeight: FontWeight.bold,
+      ),
+      centerAlignModePicker: true,
+      customModePickerIcon: const SizedBox(),
+      selectedDayTextStyle: dayTextStyle.copyWith(color: Colors.white),
+      dayTextStylePredicate: ({required date}) {
+        TextStyle? textStyle;
+        if (date.weekday == DateTime.saturday ||
+            date.weekday == DateTime.sunday) {
+          textStyle = weekendTextStyle;
+        }
+        if (DateUtils.isSameDay(date, DateTime(2021, 1, 25))) {
+          textStyle = anniversaryTextStyle;
+        }
+        return textStyle;
+      },
+      dayBuilder: ({
+        required date,
+        textStyle,
+        decoration,
+        isSelected,
+        isDisabled,
+        isToday,
+      }) {
+        Widget? dayWidget;
+        if (date.day % 3 == 0 && date.day % 9 != 0) {
+          dayWidget = Container(
+            decoration: decoration,
+            child: Center(
+              child: Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Text(
+                    MaterialLocalizations.of(context).formatDecimal(date.day),
+                    style: textStyle,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 27.5),
+                    child: Container(
+                      height: 4,
+                      width: 4,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: isSelected == true
+                            ? Colors.white
+                            : Colors.grey[500],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+        return dayWidget;
+      },
+      yearBuilder: ({
+        required year,
+        decoration,
+        isCurrentYear,
+        isDisabled,
+        isSelected,
+        textStyle,
+      }) {
+        return Center(
+          child: Container(
+            decoration: decoration,
+            height: 36,
+            width: 72,
+            child: Center(
+              child: Semantics(
+                selected: isSelected,
+                button: true,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      year.toString(),
+                      style: textStyle,
+                    ),
+                    if (isCurrentYear == true)
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        margin: const EdgeInsets.only(left: 5),
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.redAccent,
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+              final values = await showCalendarDatePicker2Dialog(
+                context: context,
+                config: config,
+                dialogSize: const Size(325, 400),
+                borderRadius: BorderRadius.circular(15),
+                value: _dialogCalendarPickerValue,
+                dialogBackgroundColor: Colors.white,
+              );
+              if (values != null) {
+                // ignore: avoid_print
+                print(_getValueText(
+                  config.calendarType,
+                  values,
+                ));
+                
+                setState(() {
+                  _dialogCalendarPickerValue = values;
+                   dateSelected = true;
+                  dateResponse = _getValueText(
+                    config.calendarType,
+                    values,
+                  );
+                  //dateResponse = values;
+                });
+              }
+            },
+            child: const Text('Select Dates'),
+          ),
+                    ),
                     SizedBox(height: 16),
                     Container(
                       decoration: BoxDecoration(
@@ -68,8 +324,8 @@ class SantoriniIslandPage extends StatelessWidget {
                         children: [
                           InfoTile(
                             icon: Icons.calendar_today,
-                            title: firstDate,
-                            subtitle: '5 Days',
+                            title: "Date" ,
+                            subtitle: dateSelected ? dateResponse: "Select Date Above" ,
                           ),
                           InfoTile(
                             icon: Icons.star,
@@ -205,3 +461,4 @@ class InfoTile extends StatelessWidget {
     );
   }
 }
+
